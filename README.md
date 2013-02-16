9140549a0fdeef7d4cd56fbc4abf154b2c5e3564
Chris James <git@etcet.net>
2013-02-15 19:46:27 -0600

    fixed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-csf-allow-pingdom-probes
+csf_allow_pingdom_probes
 ========================
 Get Pingdom probe IPs and add them to the CSF (ConfigServer Firewall) allow list. Meant to be run as a daily cronjob. For example:
 
-    32 2 * * * /root/update-pingdom-probes.sh > /dev/null 2>&1
+    32 2 * * * /root/update_pingdom_probes.sh > /dev/null 2>&1
--- update_pingdom_probes.sh
+++ update_pingdom_probes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Update the pingdom firewall rules based on their feed
 
-# rotate pingdom ip list (keep 7 days)
+# rotate pingdom ip list (keep 8 days)
 LIST=$(ls -r /root/pingdom_ips*);
 for i in $LIST; do
 	# get index of file
@@ -11,7 +11,7 @@ for i in $LIST; do
 	if [ $INDEX = "/root/pingdom_ips" ]; then
 		NEW=$INDEX.0
 		mv $i $NEW
-	# remove files with index > .6 (keep 7 files)
+	# remove files with index > 6 (keep 8 files)
 	elif [ $INDEX -gt 6 ]; then
 		rm $i
 	# increment index for all other files
@@ -23,20 +23,16 @@ for i in $LIST; do
 done
 
 # get pingdom ips from their rss feed
-/usr/bin/wget https://www.pingdom.com/rss/probe_servers.xml -O /root/probe_servers.xml -o /dev/null
-/bin/cat /root/probe_servers.xml | grep IP | sed -e 's/.*IP: //g' | sed -e 's/; Host.*//g' | grep -v IP > /root/pingdom_ips
+wget https://www.pingdom.com/rss/probe_servers.xml -O /root/probe_servers.xml -o /dev/null
+cat /root/probe_servers.xml | grep IP | sed -e 's/.*IP: //g' | sed -e 's/; Host.*//g' | grep -v IP > /root/pingdom_ips
 
 # if old lists do not exist, just allow all ips
 if [ ! -f /root/pingdom_ips.0 ]; then
-	/bin/cat /root/pingdom_ips | /usr/bin/xargs -n 1 /usr/sbin/csf -a
+	cat /root/pingdom_ips | xargs -n 1 csf -a
 else
 	# if there any differences between previous and current list, replace ips in allow list
-	DIFF=$(diff /root/pingdom_ips /root/pingdom_ips.0)
-	if ! /usr/bin/diff -q /root/pingdom_ips /root/pingdom_ips.0 > /dev/null; then
-		/bin/cat /root/pingdom_ips.0 | /usr/bin/xargs -n 1 /usr/sbin/csf -ar
-		/bin/cat /root/pingdom_ips | /usr/bin/xargs -n 1 /usr/sbin/csf -a
+	if ! diff -q /root/pingdom_ips /root/pingdom_ips.0 > /dev/null; then
+		cat /root/pingdom_ips.0 | xargs -n 1 csf -ar
+		cat /root/pingdom_ips | xargs -n 1 csf -a
 	fi
 fi
-
-# restart csf rules
-/usr/sbin/csf --restart
